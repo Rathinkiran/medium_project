9f7339b5d57b81b757f14990fada6ce34be5bffb
My name <me@example.com>
2024-06-16 00:06:05 +0530

    Updated the database url with aiven database and now completely working fine

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -2,7 +2,7 @@ import { Hono } from "hono";
 import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
 import {decode,sign,verify} from "hono/jwt"
-import { SignupInput, signinInput, signupInput } from '@rathinkiran/medium-common2'
+import { signinInput, signupInput } from '@rathinkiran/medium-common2'
 
 
 
--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,2 +1,6 @@
-export const BACKEND_URL="https://backend.rathinvj07.workers.dev"
+export const BACKEND_URL="http://127.0.0.1:8787"
+
+//https://backend.rathinvj07.workers.dev
+//http://127.0.0.1:8787
+
 
